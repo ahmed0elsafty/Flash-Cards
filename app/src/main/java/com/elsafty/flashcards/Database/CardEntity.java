fef347fa4f038b5694f7aecc4ffacaabe6934b2d
El-Safty <afawzy629@gmail.com>
2020-06-13 14:51:38 +0200

    Create Entity classes

---
+++ app/src/main/java/com/elsafty/flashcards/Database/CardEntity.java
@@ -0,0 +1,86 @@
+package com.elsafty.flashcards.Database;
+
+import java.util.Date;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.Ignore;
+import androidx.room.Index;
+import androidx.room.PrimaryKey;
+
+@Entity(
+        tableName = "cards",
+        foreignKeys = {
+                @ForeignKey(entity = SubjectEntity.class,
+                        parentColumns = "subject_id",
+                        childColumns = "card_parent_id",
+                        onDelete = ForeignKey.CASCADE)},
+        indices = @Index(value = "card_parent_id")
+)
+class CardEntity {
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "card_id")
+    private int mId;
+    @ColumnInfo(name = "card_parent_id")
+    private int mParentId;
+    private String mFrontScreen;
+    private String mBackScreen;
+    private Date mDate;
+
+    public CardEntity(int mId, int mParentId, String mFrontScreen, String mBackScreen, Date mDate) {
+        this.mId = mId;
+        this.mParentId = mParentId;
+        this.mFrontScreen = mFrontScreen;
+        this.mBackScreen = mBackScreen;
+        this.mDate = mDate;
+    }
+
+    @Ignore
+    public CardEntity(int mParentId, String mFrontScreen, String mBackScreen, Date mDate) {
+        this.mParentId = mParentId;
+        this.mFrontScreen = mFrontScreen;
+        this.mBackScreen = mBackScreen;
+        this.mDate = mDate;
+    }
+
+    public int getmId() {
+        return mId;
+    }
+
+    public void setmId(int mId) {
+        this.mId = mId;
+    }
+
+    public int getmParentId() {
+        return mParentId;
+    }
+
+    public void setmParentId(int mParentId) {
+        this.mParentId = mParentId;
+    }
+
+    public String getmFrontScreen() {
+        return mFrontScreen;
+    }
+
+    public void setmFrontScreen(String mFrontScreen) {
+        this.mFrontScreen = mFrontScreen;
+    }
+
+    public String getmBackScreen() {
+        return mBackScreen;
+    }
+
+    public void setmBackScreen(String mBackScreen) {
+        this.mBackScreen = mBackScreen;
+    }
+
+    public Date getmDate() {
+        return mDate;
+    }
+
+    public void setmDate(Date mDate) {
+        this.mDate = mDate;
+    }
+}
---
+++ app/src/main/java/com/elsafty/flashcards/Database/SubjectEntity.java
@@ -0,0 +1,65 @@
+package com.elsafty.flashcards.Database;
+
+import java.util.Date;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "subjects")
+class SubjectEntity {
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "subject_id")
+    private int mId;
+    private String mTitle;
+    private Date mDate;
+    private Integer mColor;
+
+    public SubjectEntity(@NonNull int mId, String mTitle, Date mDate, Integer mColor) {
+        this.mId = mId;
+        this.mTitle = mTitle;
+        this.mDate = mDate;
+        this.mColor = mColor;
+    }
+
+    @Ignore
+    public SubjectEntity(String mTitle, Date mDate, Integer mColor) {
+        this.mTitle = mTitle;
+        this.mDate = mDate;
+        this.mColor = mColor;
+    }
+
+    public int getmId() {
+        return mId;
+    }
+
+    public void setmId(int mId) {
+        this.mId = mId;
+    }
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public Date getmDate() {
+        return mDate;
+    }
+
+    public void setmDate(Date mDate) {
+        this.mDate = mDate;
+    }
+
+    public Integer getmColor() {
+        return mColor;
+    }
+
+    public void setmColor(Integer mColor) {
+        this.mColor = mColor;
+    }
+}
